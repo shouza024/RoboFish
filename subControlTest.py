eff8e6c05971bc82b240ee3cbe05d1fd1513aede
erick <erickcastillo20049@gmail.com>
2025-12-02 23:25:23 -0500

    d

--- subControlTest.py
+++ subControlTest.py
@@ -1,5 +1,8 @@
 from udp_receiver import get_udp_data
 
 while True:
-    data = get_udp_data()
-    print("Control Data",data)
+    try:
+        data = get_udp_data()
+        print("Control Data",data)
+    except Exception as e:
+        print("Error",e)
