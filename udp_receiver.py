c17ab2df34598ef381494970ca9d296f4c680c38
erick <erickcastillo20049@gmail.com>
2025-12-02 23:45:31 -0500

    d

--- udp_receiver.py
+++ udp_receiver.py
@@ -7,10 +7,8 @@ sock.bind(("0.0.0.0", UDP_PORT))
 sock.setblocking(False)
 
 def get_udp_data():
-    try:
         data, addr = sock.recvfrom(1024)
         decoded = json.loads(data.decode())
         print("UDP Received",decoded)      #temporary code comment out after confirm controller inputs are recieved
-        return json.loads(data.decode())   # returns the SAME list the controller provided
-    except:
-        return None
+        return decoded   # returns the SAME list the controller provided
+
