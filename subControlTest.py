804e5a8d381a1b6fb0fcac00b52eb97e47f93b54
erick <erickcastillo20049@gmail.com>
2025-12-03 00:39:15 -0500

    s

--- subControlTest.py
+++ subControlTest.py
@@ -4,6 +4,8 @@ import json
 UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("0.0.0.0", UDP_PORT))
+sock.setblocking(True)
+sock.settimeout(None)
 print(f"Listening on UDP port {UDP_PORT}")
 
 while True:
